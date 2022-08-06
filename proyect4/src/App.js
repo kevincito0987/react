5f43082563ba3bf8985ca94089706b3d391aa4cb
Kevin Angarita <kevinangaritacaamano@gmail.com>
2022-08-06 18:52:22 -0500

    La base del indicador de tareas.

---
+++ proyect4/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { Box } from '@chakra-ui/react'
+function App() {
+  return (
+    <div className="App-tareas">
+    <Box
+    fontWeight='bold'
+    fontSize='4em'
+    color='blue.700'
+    >
+    <em>
+    <h1>Mi app de tareas.</h1>
+    </em>
+    </Box>
+    <Box>hi</Box>
+    </div>
+  );
+}
+
+export default App;
